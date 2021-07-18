51084f2b6225952eb971060d0f18265c69df1bc0
Phil Schumm <pschumm@uchicago.edu>
2021-07-18 16:52:08 -0500

    Add authentication based on project-specific API key

--- redcap_booster/main.py
+++ redcap_booster/main.py
@@ -1,6 +1,6 @@
 """API for providing external services to REDCap"""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from starlette.requests import Request
 from redcap_booster import config
 
@@ -8,17 +8,23 @@ plugins = config.plugins
 app = FastAPI()
 
 @app.post('/')
-async def root(request: Request):
+async def root(request: Request, key: str = None):
     
     # Using Request explicitly here since the name of the [instrument]_complete
     # parameter is unknown ahead of time.
     # See https://www.starlette.io/requests/ for more information.
     context = await request.form()
     
+    # Require API key for authentication
+    pid = context['project_id']
+    api_key = getattr(config.settings, f'key_{pid}', 'Auth-Not-Yet-Configured')
+    if api_key != key:
+        raise HTTPException(status_code=401, detail='API key missing or invalid')
+    
     for service in plugins.list_plugins():
         
         pids = config.settings.pids
-        if context['project_id'] in pids:
+        if pid in pids:
             p_settings = getattr(config.settings,
                                  f"{service}_{context['project_id']}")
             form_triggers = p_settings['form_triggers']
