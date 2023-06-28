eee0f95b027fe871f8fd37eaab86b98510a503ad
Phil Schumm <pschumm@uchicago.edu>
2023-06-28 07:42:50 -0500

    Add refresh-ids subcommand to id-gen

--- redcap_booster/services/id_gen/cli.py
+++ redcap_booster/services/id_gen/cli.py
@@ -3,6 +3,9 @@
 import click
 import csv
 import sys
+from redcap_booster import config, redcap_api
+from redcap_booster.services import id_gen
+import json
 
 @click.command()
 @click.argument('pid')
@@ -67,4 +70,36 @@ def export_map(db, pid, filename, exclude_unused):
     csv_file.writerow(('id','record'))
     csv_file.writerows(map)
 
-commands = [load_ids, import_map, export_map]
+@click.command()
+@click.argument('pid')
+@click.argument('redcap_url')
+@click.pass_obj
+def refresh_ids(db, pid, redcap_url):
+    """Refresh all IDs for a given project
+    
+    May be used to assign IDs for existing records that do not yet have them.
+    """
+    p_settings = getattr(config.settings, f'{id_gen.service}_{pid}')
+    record_id = p_settings.get('record_id', 'record_id')
+    id_field = p_settings['id_field']
+    
+    context = dict(project_id=pid, redcap_url=redcap_url)
+    payload = {'content':'record', 'format':'json', 'fields':[record_id]}
+    records = redcap_api(id_gen.service, config, context, payload, payload).json()
+    
+    data = []
+    done = []
+    for record in records:
+        if record[record_id] not in done:
+            data.append({record_id:record[record_id],
+                         id_field:db.get_id(pid, record[record_id])})
+            done.append(record[record_id])
+    
+    payload = {'content':'record',
+               'format':'json',
+               'data':json.dumps(data, separators=(',',':'))}
+    
+    result = redcap_api(id_gen.service, config, context, payload, payload)
+    print(f'{result.json()["count"]} records refreshed')
+
+commands = [load_ids, import_map, export_map, refresh_ids]
